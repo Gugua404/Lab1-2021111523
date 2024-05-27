caca8df1bf48e54122ba9dcae133595ef7df23f7
gugua321 <13160580+gugua321@user.noreply.gitee.com>
2024-05-27 16:56:17 +0800

    Updated java_text

--- src/main/java/org/example/DirectedGraph.java
+++ src/main/java/org/example/DirectedGraph.java
@@ -455,6 +455,7 @@ public class DirectedGraph {
         }
     }
     public static void main(String[] args) throws IOException {
+        // First Change For Git Test!
         Scanner scanner = new Scanner(System.in);
 
         String content = readFileAsString("files/1.txt");
